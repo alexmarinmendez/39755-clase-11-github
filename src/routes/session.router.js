18de2c697811dab13aecee93f54f26d5a520d14e
Alex Marin Mendez <alexmarinmendez@gmail.com>
2023-05-20 13:21:11 -0500

    feat: add logout

--- src/routes/session.router.js
+++ src/routes/session.router.js
@@ -3,17 +3,26 @@ import passport from 'passport'
 
 const router = Router()
 
-router.get('/', (req, res) => res.render('index'))
+router.get('/', (req, res) => res.render('index', { user: req.user.first_name }))
 
 router.get('/login', (req, res) => res.render('login'))
 
-router.get('/github', passport.authenticate('github', { scope: ["user:email"] }), (req, res) => {})
+router.get('/github', passport.authenticate('github', { scope: ["user:email"] }), (req, res) => { })
 
-router.get('/githubcallback', 
-    passport.authenticate('github', { failureRedirect: '/login'}), 
-    async(req, res) => {
+router.get('/githubcallback',
+    passport.authenticate('github', { failureRedirect: '/api/session/login' }),
+    async (req, res) => {
         req.session.user = req.user
-        res.redirect('/')
+        res.redirect('/api/session')
     })
+
+
+router.get('/logout', function (req, res) {
+    req.logout(function (err) {
+        if (err) { return res.send({ error: 'error al logout'}); }
+        res.redirect('/api/session/login');
+    });
+});
+
 
 export default router
